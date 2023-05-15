0825bab7d38e92bce915237faecc05dcbe4c544b
Levi <megaraidsserver@gmail.com>
2023-05-15 16:55:18 +0000

    Update pusher.ts

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -2,10 +2,10 @@ import PusherServer from 'pusher'
 import PusherClient from 'pusher-js'
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_SECRET!,
-  cluster: 'us2',
+  appId: "1599788",
+  key: "d8525b46b3d7e37db141",
+  secret: "e156b3357ce62b43372e",
+  cluster: "us2",
   useTLS: true,
 });
 
